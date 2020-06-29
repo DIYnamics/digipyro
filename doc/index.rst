0be0bdd4f7aa0a373f75bc4a28e1600887fe8dfb
Spencer Hill <shill@ldeo.columbia.edu>
2020-06-29 10:32:24 +0000

    WIP Package restructure (#10)
    * Add sphinx-based docs pages skeletons
    * Convert README from orgmode to markdown
    * Add Apache license
    * Add setup.py
    * Delete obsolete installation script
    * Delete obsolete mathematica notebook
    * Delete obsolete instructions PDF and excess sample movies
    This reverts commit 87998bd5c4e0b9623448d0928fec911ea843323a.
    
    
    * Create scripts dir; delete excess example videos

---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. digipyro documentation master file, created by
+   sphinx-quickstart on Mon May 11 20:22:06 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to digipyro's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+setuptools.setup(
+    name="digipyro",
+    version="0.0",
+    packages=setuptools.find_packages(),
+    author="The DIYnamics Team",
+    author_email="DIYnamicsTeam@gmail.com",
+    description="Digitally Rotate a movie in Python",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "opencv-python",
+        "tk",
+    ],
+    scripts=[
+        "digipyro/scripts/digipyro.py",
+        "digipyro/scripts/synth.py"]
+    ,
+    license="Apache",
+    keywords="education",
+    url="https://github.com/DIYnamics/digipyro",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Atmospheric Science"
+    ]
+)
